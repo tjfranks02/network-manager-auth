da4976fac6878c221b2c0d3df03c77c0345d699c
Tyson Franks <tjfranks02@hotmail.com>
2023-11-04 01:03:42 +1100

    Added docstring to getPublicKey controller.

--- src/controllers/jwt.ts
+++ src/controllers/jwt.ts
@@ -1,6 +1,17 @@
 import type { Request, Response } from "express";
 import { getJWTPublicKey } from "../utils/jwt";
 
+/**
+ * Get the public key for a given JWT token ID.
+ * 
+ * Request params:
+ *   tokenId - the ID of the public key to get.
+ *  
+ * Response:
+ *   200 - the public key.
+ *   404 - the public key ID is invalid and the public key can't be found.
+ *   422 - the request is missing the public key ID.
+ */
 export const getPublicKey = async (req: Request, res: Response) => {
   const tokenId: string = req.params.tokenId;
 
