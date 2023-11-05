644c4045cd40054e1b1c3bbd93c55217d71b49f0
Tyson Franks <tjfranks02@hotmail.com>
2023-11-05 15:17:50 +1100

    Refactored jwt signing options to use keyid option built into jsonwebtoken package.

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -8,13 +8,30 @@ import { JWT_EXPIRY_TIME, JWT_SIGNING_ALGO, REFRESH_TOKEN_EXPIRY_TIME } from "..
  * Get the options used to sign JWT tokens.
  */
 const getJWTSigningOptions = (): SignOptions => {
+
+//   export interface SignOptions {
+//     algorithm?: Algorithm | undefined;
+//     keyid?: string | undefined;
+//     /** expressed in seconds or a string describing a time span [zeit/ms](https://github.com/zeit/ms.js).  Eg: 60, "2 days", "10h", "7d" */
+//     expiresIn?: string | number | undefined;
+//     /** expressed in seconds or a string describing a time span [zeit/ms](https://github.com/zeit/ms.js).  Eg: 60, "2 days", "10h", "7d" */
+//     notBefore?: string | number | undefined;
+//     audience?: string | string[] | undefined;
+//     subject?: string | undefined;
+//     issuer?: string | undefined;
+//     jwtid?: string | undefined;
+//     mutatePayload?: boolean | undefined;
+//     noTimestamp?: boolean | undefined;
+//     header?: JwtHeader | undefined;
+//     encoding?: string | undefined;
+//     allowInsecureKeySizes?: boolean | undefined;
+//     allowInvalidAsymmetricKeyTypes?: boolean | undefined;
+// }
+
   let signingOptions: SignOptions = {
     algorithm: JWT_SIGNING_ALGO,
     expiresIn: JWT_EXPIRY_TIME,
-    header: {
-      alg: "RS256",
-      kid: "1"
-    }
+    keyid: "1"
   };
 
   return signingOptions;
